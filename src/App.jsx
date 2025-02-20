6ed12ab61855fe6e8978753beea52adbedbbd2b5
watanabe-taichi <>
2025-02-20 17:15:50 +0900

    確認用

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,5 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import { DefaultLayout } from "./components/layouts/DefaultLayout";
-import { Home } from "./components/pages/Home";
-import { About } from "./components/pages/About";
+function App() {
+  return <h1>Test</h1>;
+}
 
-export const App = () => {
-  return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <DefaultLayout>
-            <Home />
-          </DefaultLayout>
-        }
-      />
-      <Route
-        path="/about"
-        element={
-          <DefaultLayout>
-            <About />
-          </DefaultLayout>
-        }
-      />
-    </Routes>
-  );
-};
+export default App;
