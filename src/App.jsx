d40fa2e56e4d9ba1aa75c02955dffc5af106b831
watanabe-taichi <>
2025-02-20 17:36:11 +0900

    routerの設定

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { DefaultLayout } from "./components/layouts/DefaultLayout";
 import { Home } from "./components/pages/Home";
 import { About } from "./components/pages/About";
 
 export const App = () => {
   return (
-    <Router basename="/nJOY">
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <DefaultLayout>
-              <Home />
-            </DefaultLayout>
-          }
-        />
-        <Route
-          path="/about"
-          element={
-            <DefaultLayout>
-              <About />
-            </DefaultLayout>
-          }
-        />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <DefaultLayout>
+            <Home />
+          </DefaultLayout>
+        }
+      />
+      <Route
+        path="/about"
+        element={
+          <DefaultLayout>
+            <About />
+          </DefaultLayout>
+        }
+      />
+    </Routes>
   );
 };
--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import { App } from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/nJOY">
       <App />
     </BrowserRouter>
   </React.StrictMode>
