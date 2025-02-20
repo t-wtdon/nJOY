c2bf40de0d4518d6d91bd1d27bb8ff5d4776413b
watanabe-taichi <>
2025-02-20 17:32:05 +0900

    routerの設定

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
-function App() {
-  return <h1>Test</h1>;
-}
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { DefaultLayout } from "./components/layouts/DefaultLayout";
+import { Home } from "./components/pages/Home";
+import { About } from "./components/pages/About";
 
-export default App;
+export const App = () => {
+  return (
+    <Router basename="/nJOY">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <DefaultLayout>
+              <Home />
+            </DefaultLayout>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <DefaultLayout>
+              <About />
+            </DefaultLayout>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+};
