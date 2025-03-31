8e61c4b09eea0b5a674ad3c7d31952d72803d329
watanabe-taichi <>
2025-03-31 14:20:36 +0900

    修正

--- src/components/common/pages/Genki/Point.tsx
+++ src/components/common/pages/Genki/Point.tsx
@@ -8,7 +8,6 @@ const point2 = require("../../../../assets/images/genki/point2.png");
 const point3 = require("../../../../assets/images/genki/point3.png");
 const point4 = require("../../../../assets/images/genki/point4.png");
 const point5 = require("../../../../assets/images/genki/point5.png");
-const point5_a = require("../../../../assets/images/genki/point5.jpg");
 const point6 = require("../../../../assets/images/genki/point6.png");
 
 const Container = styled.section`
@@ -40,6 +39,7 @@ const Item = styled.li`
 
   @media (max-width: ${breakpoints.sp}) {
     width: 100%;
+    margin: 0;
   }
 `;
 
@@ -116,7 +116,6 @@ export const Point = () => {
         </Item>
         <Item>
           <Image src={point5} alt="" />
-          <Image src={point5_a} alt="" />
           <Title>介護と医療の架け橋に</Title>
           <Text>
             介護や医療には、さまざまな職種の連携が必要です。
