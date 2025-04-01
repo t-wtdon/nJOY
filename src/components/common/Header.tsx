985a655b30023219a71ca6345e509c4807ea43a4
watanabe-taichi <>
2025-04-01 18:55:02 +0900

    headerの透過

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -23,7 +23,7 @@ const HeaderWrap = styled.header`
   width: 100%;
   z-index: 1000;
   box-sizing: border-box;
-  background: rgba(255, 255, 255, 1);
+  background: rgba(255, 255, 255, 0.7);
 `;
 
 const Logo = styled(Link)`
