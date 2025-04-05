69ac573b29099bb5e3550d7189d5a0e93afa23e0
watanabe-taichi <>
2025-04-05 14:44:17 +0900

    住所変更の部分の改行

--- src/components/pages/Company.tsx
+++ src/components/pages/Company.tsx
@@ -259,6 +259,7 @@ export const Company = () => {
           <Head>所在地</Head>
           <Data>
             <SmallText>げんき訪問看護ステーション: </SmallText>
+            <br />
             〒211-0005 神奈川県川崎市中原区新丸子町715-10 パークサイドパレス1F
           </Data>
         </Row>
