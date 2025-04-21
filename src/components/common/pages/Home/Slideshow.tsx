e7ec6e36a33bbe8d1d6705d90ed7825d23a4d956
watanabe-taichi <>
2025-04-21 10:09:54 +0900

    会社概要のceoの写真差し替え、スライドショーの写真追加

--- src/components/common/pages/Home/Slideshow.tsx
+++ src/components/common/pages/Home/Slideshow.tsx
@@ -8,10 +8,12 @@ const slide5 = require("../../../../assets/images/slide/slide5.jpg");
 const slide6 = require("../../../../assets/images/slide/slide6.jpg");
 const slide7 = require("../../../../assets/images/slide/slide7.jpg");
 const slide8 = require("../../../../assets/images/slide/slide8.jpg");
+const slide9 = require("../../../../assets/images/slide/slide9.jpg");
 const slide10 = require("../../../../assets/images/slide/slide10.jpg");
 const slide11 = require("../../../../assets/images/slide/slide11.jpg");
 const slide12 = require("../../../../assets/images/slide/slide12.jpg");
 const slide13 = require("../../../../assets/images/slide/slide13.jpg");
+const slide14 = require("../../../../assets/images/slide/slide14.jpg");
 const slide15 = require("../../../../assets/images/slide/slide15.jpg");
 const slide16 = require("../../../../assets/images/slide/slide16.jpg");
 
@@ -23,10 +25,12 @@ const images = [
   slide6,
   slide7,
   slide8,
+  slide9,
   slide10,
   slide11,
   slide12,
   slide13,
+  slide14,
   slide15,
   slide16,
 ];
